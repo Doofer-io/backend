1048b4834530ded132f9db75a5b09dcc7f7badaa
Artur Demenskiy <artur.demenskiy@elrom.org>
2023-08-16 18:45:53 +0300

    fix: clean code

--- src/modules/auth/google/google.strategy.ts
+++ src/modules/auth/google/google.strategy.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-google-oauth20';
-import { PrismaService } from 'src/shared/services/prisma.service';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
